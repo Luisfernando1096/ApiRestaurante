82460fddcf34b72848487e4b868fd57c9b2ad6d0
fruiz <fruiz@LAPTOP-QBB4AB87>
2023-10-04 12:00:55 -0600

    Modificada clase PedidoDetalle

--- ApiRestaurante.Model/CLS/PedidoDetalle.cs
+++ ApiRestaurante.Model/CLS/PedidoDetalle.cs
@@ -8,20 +8,38 @@ namespace ApiRestaurante.Model.CLS
 {
     public class PedidoDetalle
     {
-        int idPedido;
+        int idDetalle;
+        bool cocinando;
+        String extras;
+        String horaEntregado;
+        String horaPedido;
+        int idCocinero;
+        String cocinero;
         int idProducto;
+        String nombre;
+        int idPedido;
         int cantidad;
         double precio;
-        String nombre;
         double subTotal;
-        DateTime fecha;
+        String grupo;
+        String usuario;
+        String fecha;
 
-        public int IdPedido { get => idPedido; set => idPedido = value; }
+        public int IdDetalle { get => idDetalle; set => idDetalle = value; }
+        public bool Cocinando { get => cocinando; set => cocinando = value; }
+        public string Extras { get => extras; set => extras = value; }
+        public string HoraEntregado { get => horaEntregado; set => horaEntregado = value; }
+        public string HoraPedido { get => horaPedido; set => horaPedido = value; }
+        public int IdCocinero { get => idCocinero; set => idCocinero = value; }
+        public string Cocinero { get => cocinero; set => cocinero = value; }
         public int IdProducto { get => idProducto; set => idProducto = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int IdPedido { get => idPedido; set => idPedido = value; }
         public int Cantidad { get => cantidad; set => cantidad = value; }
         public double Precio { get => precio; set => precio = value; }
-        public string Nombre { get => nombre; set => nombre = value; }
         public double SubTotal { get => subTotal; set => subTotal = value; }
-        public DateTime Fecha { get => fecha; set => fecha = value; }
+        public string Grupo { get => grupo; set => grupo = value; }
+        public string Usuario { get => usuario; set => usuario = value; }
+        public string Fecha { get => fecha; set => fecha = value; }
     }
 }
